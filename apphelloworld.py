c46e30453a9be55d04b701a35d7d02b44e9719b4
algaedesma <algaedesma2004@gmail.com>
2025-05-28 14:48:14 +0700

    Update apphelloworld.py

--- apphelloworld.py
+++ apphelloworld.py
@@ -11,3 +11,12 @@ import streamlit as st
 
 st.title("Hello World")
 st.write("Ini adalah aplikasi Streamlit pertama saya!")
+
+st.set_page_config(page_title="Insurance Classifier", layout="wide")
+page = st.sidebar.selectbox("Select a page", ["Data Description", "Prediction", "About Naive Bayes"])
+
+import streamlit as st
+
+st.set_page_config(page_title="Insurance Classifier", layout="wide")
+st.title("Tes")
+page = st.sidebar.selectbox("Select a page", ["Data Description", "Prediction", "About Naive Bayes"])
