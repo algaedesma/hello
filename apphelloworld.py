6fd222ff5d2ff0f92c8be93888a35172b7ca06f0
algaedesma <algaedesma2004@gmail.com>
2025-05-28 14:49:28 +0700

    Update apphelloworld.py

--- apphelloworld.py
+++ apphelloworld.py
@@ -15,8 +15,8 @@ st.write("Ini adalah aplikasi Streamlit pertama saya!")
 st.set_page_config(page_title="Insurance Classifier", layout="wide")
 page = st.sidebar.selectbox("Select a page", ["Data Description", "Prediction", "About Naive Bayes"])
 
-import streamlit as st
-
 st.set_page_config(page_title="Insurance Classifier", layout="wide")
 st.title("Tes")
 page = st.sidebar.selectbox("Select a page", ["Data Description", "Prediction", "About Naive Bayes"])
+
+
